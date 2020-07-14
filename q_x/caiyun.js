432ee7c8869d71886212c1648e006f001c8bc039
dcty <dcty@qq.com>
2020-07-14 19:34:21 +0800

    Update caiyun.js
    1

--- q_x/caiyun.js
+++ q_x/caiyun.js
@@ -1,8 +1,11 @@
 let body = JSON.parse($response.body)
 body.result.xy_vip_expire = 0
+body.result.vip_expired_at = 0
+body.result.vip_type = "s"
 body.result.is_vip = true
 body.result.vip_expired_at = 0
 body.result.svip_expired_at = 4070883661.2019770145
 body.result.wt.vip.enable = true
 body.result.wt.vip.svip_expired_at = 4070883661.2019770145
+body.result.score = 200000
 $done({ body: JSON.stringify(body) })
